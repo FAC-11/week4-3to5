75a66a3517c20652a6871f2bea0947c098536f9c
RogeredBacon <jinglis12@googlemail.com>
2017-07-19 15:43:56 +0100

    Added index function to the handler #19

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+
 const search = require('./search.js');
 
-const index = function(request, response, filePath) {
-  
+const index = function (request, response) {
+  const filePath = path.join(__dirname, '..', 'public', 'index.html');
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.readFile(filePath, function (error, file) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    response.end(file);
+  }
+)
 };
-const query = function(request, response, filePath) {
-  
+const query = function (request, response, filePath) {
+
 };
-const assets = function(request, response, filePath) {
-  
+const assets = function (request, response, filePath) {
+
 };
 
 module.exports = {
   index,
   query,
-  assets
+  assets,
 };
