8337c1ccc37385bdc729de98ee6d39eb7de2b702
RogeredBacon <jinglis12@googlemail.com>
2017-07-19 16:04:08 +0100

    Added asset handling function to the handler #19 + #7

--- src/handler.js
+++ src/handler.js
@@ -13,16 +13,32 @@ const index = function (request, response) {
       return;
     }
     response.end(file);
-  }
-)
+  },
+);
 };
 const query = function (request, response, filePath) {
 
 };
 const assets = function (request, response, filePath) {
+  const contentType = {
+    css: 'text/css',
+    html: 'text/html',
+    js: 'application/javascript',
+    png: 'image/png',
+  };
 
+  const assetUrl = request.url;
+  const extension = assetUrl.split('.')[1];
+  const assetFilePath = path.join(__dirname, '..', 'public', assetUrl);
+  response.writeHead(200, { 'Content-Type': contentType[extension] });
+  fs.readFile(assetFilePath, function (error, file) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    response.end(file);
+  });
 };
-
 module.exports = {
   index,
   query,
