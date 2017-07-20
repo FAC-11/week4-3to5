c877cfff741aaebb08e25a54b3f070f137fd0d1c
mineshmshah <mineshmshah@hotmail.co.uk>
2017-07-20 11:51:59 +0100

    Created dom.js + added theme to index + added validation + event listener + new value check #22 #6 #7

---
+++ public/dom.js
@@ -0,0 +1,15 @@
+const domCallback = function(error, response) {
+  if(error) {
+    console.log(error);
+  }
+  if(response.pokeTenMatches) {
+  const resultsDom = document.getElementsByID('search_bar');
+  resultsDom.innerHTML = '';
+  response.pokeTenMatches.forEach(function(pokeName) {
+    const option = document.createElement(option);
+    option.value = pokeName;
+    resultsDom.appendChild(option);
+  })
+
+  }
+}
--- public/main.js
+++ public/main.js
@@ -1,8 +1,41 @@
-(function() {
-  var results = ['pikachu', 'coffin', 'jigglypuff', 'squirtle', 'ekans'];
-  var optionDrops = document.getElementsByClassName('option');
 
-  Array.prototype.forEach.call(optionDrops, function(optionDrop, index) {
-    optionDrop.textContent = results[index];
-  });
+const domInput = document.getElementsByID('search_input');
+const searchRequest = (function() {
+  let oldValue = '';
+   return function(newValue) {
+     if(newValue !== oldValue) {
+       pokeRequest('/search?q=' + newValue, domCallback);
+     } else {
+       domCallback(null, '');
+     }
+     oldValue = newValue;
+   }
+
 })();
+
+
+const searchTerm = (function() {
+  let checkSymbols = function(string) {
+    return string.match(/[^a-z]/gi);
+  }
+  return function(inputText, callback) {
+    if(inputText && !checkSymbols(inputText)) {
+      callback(inputText);
+    } else {
+      callback();
+    }
+  }
+})();
+
+domInput.addEventListener('keyup', function(event) {
+  searchTerm(event.target.value, searchRequest);
+})
+
+// (function() {
+//   var results = ['pikachu', 'coffin', 'jigglypuff', 'squirtle', 'ekans'];
+//   var optionDrops = document.getElementsByClassName('option');
+//
+//   Array.prototype.forEach.call(optionDrops, function(optionDrop, index) {
+//     optionDrop.textContent = results[index];
+//   });
+// })();
